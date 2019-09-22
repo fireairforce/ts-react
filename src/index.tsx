c70aa139c738f9b08dc5603aa6f8f995402eedef
fireairforce <1344492820@qq.com>
2019-09-22 15:11:49 +0800

    feat:init all project

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from "react-redux";
-import configureStore from "./store/index";
+import { Provider } from 'react-redux';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store/index';
 
 const store = configureStore();
 const Root = () => (
