744b1ef854b0d11cef3a215a88129a6849a9f56d
fireairforce <1344492820@qq.com>
2019-09-23 16:34:56 +0800

    feat:完成和ts-express的对接

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 《TypeScript 实战指南》-3
 
 里面有部分类型检测会出现`Error`，先暂时没有处理，后续会一起处理
+
+修改了一波强制的类型检测:
+
+```
+https://blog.csdn.net/m4a1k2ak47/article/details/80623955
+```
--- src/action/index.ts
+++ src/action/index.ts
@@ -1,3 +1,7 @@
+import { Dispatch } from "redux";
+
+const headers = new Headers({ "content-type": "application/json" });
+
 export const EDIT_DRAFT_ACTION_TYPE = "draft/edit";
 export const editDraftAction = (payload: IDraftState) => ({
   type: EDIT_DRAFT_ACTION_TYPE,
@@ -5,9 +9,18 @@ export const editDraftAction = (payload: IDraftState) => ({
 });
 
 export const SAVE_DRAFT_ACTION_TYPE = "draft/save";
-export const saveDraftAction = () => ({
-  type: SAVE_DRAFT_ACTION_TYPE,
-});
+export const saveDraftAction = () => {
+  return (dispatch: Dispatch, getState) => {
+    const draft = getState().draft;
+    fetch("http://localhost:3000/work-items", {
+      headers,
+      method: "post",
+      body: JSON.stringify(draft),
+    }).then(() => {
+      dispatch(resetDraftAction());
+    });
+  };
+};
 
 export const RESET_DRAFT_ACTION_TYPE = "draft/reset";
 export const resetDraftAction = () => ({
